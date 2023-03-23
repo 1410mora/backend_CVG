ae624e9f1fec1f47195402ec68bdd22c5798e64f
mora1410 <124624558+mora1410@users.noreply.github.com>
2023-03-23 17:19:30 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/cvg/CvgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvgApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfolio/cvg/Service/ImpPersonaService.java
@@ -0,0 +1,37 @@
+
+package com.portfolio.cvg.Service;
+
+import com.portfolio.cvg.Entity.Persona;
+import com.portfolio.cvg.Interface.IPersonaService;
+import com.portfolio.cvg.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+ public class ImpPersonaService implements IPersonaService {
+    @Autowired IPersonaRepository ipersonarepository;
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonarepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonarepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonarepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonarepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
