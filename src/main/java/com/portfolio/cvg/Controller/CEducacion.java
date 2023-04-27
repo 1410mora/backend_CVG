68de69027038820e43624d3115a26bf15af0472e
mora1410 <ceci.gomez.1410@gmail.com>
2023-04-27 16:15:39 -0300

    Modificacion cross origin Frontend

--- src/main/java/com/portfolio/cvg/Controller/CEducacion.java
+++ src/main/java/com/portfolio/cvg/Controller/CEducacion.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-cecy.web.app/")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
